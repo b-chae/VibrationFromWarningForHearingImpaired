096b235dea894463285aa8a8dec97a27f557071a
asomeJay <museum114@naver.com>
2020-04-29 14:59:59 +0900

    Squash : /Server MP3 TEST COMPLETE
    commit cef924d261e914836ce17fe2a04829d05d513eef
    Author: asomeJay <museum114@naver.com>
    Date:   Wed Apr 29 14:57:59 2020 +0900
    
        :white_check_mark: MP3 Form-Data POST TEST COMPLETE
    
    commit d0c6a3240ae903b595095f49c963270a93e7850f
    Author: asomeJay <museum114@naver.com>
    Date:   Wed Apr 29 13:59:54 2020 +0900
    
        :arrow_up: form-data add
    
    commit bcde03a7dc338a1d0d0d937ef1ccc35fae07e5f0
    Author: asomeJay <museum114@naver.com>
    Date:   Wed Apr 29 13:59:30 2020 +0900
    
        :bug: no code bug fix
    
    commit d0fb26b0ae70d6d3bd969fe386c5df89852b7aff
    Merge: 5bf49cd b3871ac
    Author: asomeJay <45970468+asomeJay@users.noreply.github.com>
    Date:   Tue Apr 28 15:05:28 2020 +0900
    
        Merge pull request #28 from asomeJay/server
    
        TEST2
    
    commit b3871ac8680ed4b47ac20048affcbef409d88f23
    Author: asomeJay <museum114@naver.com>
    Date:   Tue Apr 28 15:03:40 2020 +0900
    
        TEST2
    
    commit 5bf49cddc6f338bb13f363ff220ee430b051a984
    Merge: 2af92d2 d7c2870
    Author: asomeJay <45970468+asomeJay@users.noreply.github.com>
    Date:   Tue Apr 28 14:58:34 2020 +0900
    
        Merge pull request #27 from asomeJay/server
    
        Server
    
    commit d7c2870d8116cfcd2a81454136218ddda23d362a
    Merge: 51aeddf f1f2129
    Author: asomeJay <museum114@naver.com>
    Date:   Tue Apr 28 14:56:48 2020 +0900
    
        Merge remote-tracking branch 'origin/server' into server
    
    commit 51aeddfb310b7b39a518edc8ceeb94e492cca8f6
    Author: asomeJay <museum114@naver.com>
    Date:   Tue Apr 28 14:53:35 2020 +0900
    
        :
    
    commit 79c0fd361a6426be5f4ae18eab6329e626ae270b
    Author: asomeJay <museum114@naver.com>
    Date:   Tue Apr 28 14:43:09 2020 +0900
    
        TEST4
    
    commit 291e7470115fcc579f91da3af572c04feb6b09bd
    Author: asomeJay <museum114@naver.com>
    Date:   Tue Apr 28 14:42:55 2020 +0900
    
        TEST3
    
    commit 26e1ef180c2c657e4146dec0991fc6645ca1cbe5
    Author: asomeJay <museum114@naver.com>
    Date:   Tue Apr 28 14:42:46 2020 +0900
    
        TEST2
    
    commit 7a092e879519256e535e68ae08afd3f1f810fd56
    Author: asomeJay <museum114@naver.com>
    Date:   Tue Apr 28 14:42:32 2020 +0900
    
        TEST1

--- Server/src/signalRoutes.js
+++ Server/src/signalRoutes.js
@@ -1,9 +1,9 @@
 import express from 'express';
-import { soundPost, vibeLedGet } from '../controller/signalController';
+import { soundPost, vibeLedGet, upload } from '../controller/signalController';
 
 const signalRouter = express.Router();
 
 signalRouter.get('/', vibeLedGet);
-signalRouter.post('/', soundPost);
+signalRouter.post('/', upload.single('mp3') , soundPost);
 
 export default signalRouter;
