91354105705da0bdd5a5a57020c59d43c1652fe8
asomeJay <museum114@naver.com>
2020-05-10 18:38:45 +0900

    :hot: post bug brake out

--- Server/controller/signalController.js
+++ Server/controller/signalController.js
@@ -5,7 +5,9 @@ import spawn from 'child_process';
 
 dotenv.config();
 
-export const upload = multer({ dest: path.resolve(__dirname, 'uploads/')});
+export const upload = multer({
+    dest: path.resolve(__dirname, 'uploads/')
+});
 
 
 function vibrate() {
@@ -18,25 +20,42 @@ function led(color) {
 
 
 export const soundPost = (req, res) => {
+    console.log("SOUNDPOST");
+    console.log(req.name);
+    console.log(req.header);
+    console.log(req.headers);
+    console.log(req.req);
+    console.log(req.data);
+    console.log(req.body);
     console.log(req.file);
+    console.log(req.files);
 
+    // res?.sendStatus(200);
+
+    res.status(200).json({
+        LED: "RED",
+        vibrator : true
+    })
+    // res.json({
+    //     LED: "RED",
+    //     vibrator: true
+    // })
     // const pythonProcess = spawn.spawn('python', ["../../ML/Keras-Project-Template/main.py"]);
-    
 }
 
 export const vibeLedGet = (req, res) => {
     console.log("VIBELGET");
-    const pythonProcess = spawn.spawn('python', ["C:/Users/KimJaeWon/Desktop/Project/VibrationFromWarningForHearingImpaired/ML/Keras-Project-Template/main.py"]);
-    // const pythonProcess = spawn.spawn('python', ["C:/Users/KimJaeWon/Desktop/Project/VibrationFromWarningForHearingImpaired/pyt.py"]);
+    // const pythonProcess = spawn.spawn('python', ["C:/Users/KimJaeWon/Desktop/Project/VibrationFromWarningForHearingImpaired/ML/Keras-Project-Template/main.py"]);
+    const pythonProcess = spawn.spawn('python', ["C:/Users/KimJaeWon/Desktop/Project/VibrationFromWarningForHearingImpaired/pyt.py"]);
     var dataToSend;
     
     pythonProcess.stdout.on('data', (data) => {
-        console.log(data.toString());
+        // console.log(data.toString());
         dataToSend = data.toString();
     });
 
     pythonProcess.on('close', (code) => {
-        console.log(`${code} 464646464`);
+        // console.log(`${code} 464646464`);
         res.send(dataToSend);
     })
 
--- Server/src/signalRoutes.js
+++ Server/src/signalRoutes.js
@@ -4,6 +4,7 @@ import { soundPost, vibeLedGet, upload } from '../controller/signalController';
 const signalRouter = express.Router();
 
 signalRouter.get('/', vibeLedGet);
-signalRouter.post('/', upload.single('mp3') , soundPost);
+signalRouter.post('/', upload.any() , soundPost);
+// signalRouter.post('/', ,soundPost);
 
 export default signalRouter;
